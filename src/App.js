672419a144b736d15c597ea1457ca8c0985a76cc
understanding963852 <60366769+understanding963852@users.noreply.github.com>
2023-11-18 20:02:22 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+//수코딩 - react sandbox 설정
+//https://www.youtube.com/watch?v=BEdpzyRwkMs&t=481s
+
+// 아래는 설치하여야할 소스
+// 설정메뉴에서 File>Preferences>User Snippets
+// 중앙에  여러 옵션이 나타난다  New Global Snippets file.. 선택  클릭
+//    /vscode/snippets/Untitled.code-snippets 에서 Untitled 부분만 식별할수있는 코드로 변경해 주면 된다.  react라고 적었다.
+//그러면 화면에 그 파일이 열린다.
+//그곳에 내용을 모두 지우고 아래 경로의 코드를 복사하여  전체 붙여넣기 하면 된다.
+
+//https://raw.githubusercontent.com/ults-io/vscode-react-javascript-snippets/master/src/snippets/generated.json
+
+//rafce
+//위의 내용을 끝내면 단축키 rafce와 같은 단축키가 먹히게 된다.
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
